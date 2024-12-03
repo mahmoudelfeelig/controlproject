e7d6469a914b219a8c42e8ec35680c37c18af560
fadyyakoub <163349343+fadyyakoub@users.noreply.github.com>
2024-12-03 11:22:36 +0100

    I created SystemGraphs matlab file to plot the required diagrams for analyzing the system and it can also be used by others as a tempalate for analysing the system after adding the controllers.

---
+++ SystemGraphs.m
@@ -0,0 +1,16 @@
+step(G) %-->to get step respose of a system
+
+figure
+impulse(G) %-->to get impulse respose of a system
+
+figure
+step(Gclosed)  %-->to get step respose of a system
+
+figure
+impulse(Gclosed) %-->to get impulse respose of a system
+
+figure 
+rlocus(Gclosed)  %-->to get root locus diagram of a system
+
+figure
+bode(Gclosed)    %-->to get the bode diagram of a system
